a5bcad1bdcdf3b51e081e1ec9d7ab8998248d413
android-yanwensheng <1525896610@qq.com>
2023-12-12 18:18:13 +0800

    [master-1.1.1] 注入HttpService、KVService

--- base/src/main/java/com/willis/base/services/ServicesInject.kt
+++ base/src/main/java/com/willis/base/services/ServicesInject.kt
@@ -1,8 +1,12 @@
 package com.willis.base.services
 
 import com.willis.base.services.api.IDialogService
+import com.willis.base.services.api.IHttpService
+import com.willis.base.services.api.IKVService
 import com.willis.base.services.api.IToastService
 import com.willis.base.services.impl.DialogService
+import com.willis.base.services.impl.HttpService
+import com.willis.base.services.impl.KVService
 import com.willis.base.services.impl.ToastService
 
 
@@ -12,6 +16,16 @@ import com.willis.base.services.impl.ToastService
 val dialogService: IDialogService get() = DialogService
 
 /**
- * Toast服务
+ * Toast 服务
  */
 val toastService: IToastService get() = ToastService
+
+/**
+ * HTTP 服务
+ */
+val httpService: IHttpService get() = HttpService
+
+/**
+ * Key-Value 服务
+ */
+val kvService: IKVService get() = KVService
