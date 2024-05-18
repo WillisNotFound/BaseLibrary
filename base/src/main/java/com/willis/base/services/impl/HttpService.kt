540b04dab2c99ae8bedce74a48f2edf7880ebc55
android-yanwensheng <1525896610@qq.com>
2024-05-19 01:13:51 +0800

    [master-1.1.10] add http timeout

--- base/src/main/java/com/willis/base/services/impl/HttpService.kt
+++ base/src/main/java/com/willis/base/services/impl/HttpService.kt
@@ -4,6 +4,7 @@ import com.willis.base.services.api.IHttpService
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
+import java.util.concurrent.TimeUnit
 
 /**
  * description: IHttpService 实现类
@@ -11,7 +12,12 @@ import okhttp3.Response
  * @date: 2023/12/12
  */
 internal object HttpService : IHttpService {
-    private val sOkHttpClient = OkHttpClient()
+    private val sOkHttpClient = OkHttpClient.Builder()
+        .callTimeout(10, TimeUnit.SECONDS)
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(20, TimeUnit.SECONDS)
+        .writeTimeout(20, TimeUnit.SECONDS)
+        .build()
 
     override suspend fun send(request: Request): Response {
         return sOkHttpClient.newCall(request).execute()
